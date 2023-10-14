e7c5c45c95cb83a569ce7dd04edabaa28fbdef36
DanP-O <daniel.parish.olson@gmail.com>
2023-10-14 15:33:55 -0500

    Initial Commit

---
+++ TodoPal/TodoPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoPalApp.swift
+//  TodoPal
+//
+//  Created by Daniel Parish-Olson on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
